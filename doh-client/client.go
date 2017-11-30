c4412e2993342211627ff87aac4cbebdba42d8c1
Star Brilliant <coder@poorlab.com>
2017-11-30 15:53:13 +0800

    Enable Cookie, which might have better CDN cache performance

--- doh-client/client.go
+++ doh-client/client.go
@@ -101,6 +101,7 @@ func NewClient(addr, upstream string, bootstraps []string, timeout uint, noECS, 
 		DualStack: true,
 		Resolver: bootResolver,
 	}).DialContext
+	// Most CDNs require Cookie support to prevent DDoS attack
 	cookieJar, err := cookiejar.New(nil)
 	if err != nil { return nil, err }
 	c.httpClient = &http.Client {
