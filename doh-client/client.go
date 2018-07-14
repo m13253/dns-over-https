cfa078a664685d5f43b128ba3db66501414a28e3
lightsing <light.tsing@gmail.com>
2018-07-14 13:39:48 +0800

    move overriding

--- doh-client/client.go
+++ doh-client/client.go
@@ -267,16 +267,6 @@ func (c *Client) findClientIP(w dns.ResponseWriter, r *dns.Msg) (ednsClientAddre
 	if c.conf.NoECS {
 		return net.IPv4(0, 0, 0, 0), 0
 	}
-	if ip, _, err := net.ParseCIDR(c.conf.EDNSSubnet); err == nil {
-		if ipv4 := ip.To4(); ipv4 != nil {
-			ednsClientAddress = ipv4.Mask(ipv4Mask24)
-			ednsClientNetmask = 24
-		} else {
-			ednsClientAddress = ip.Mask(ipv6Mask48)
-			ednsClientNetmask = 48
-		}
-		return
-	}
 	if opt := r.IsEdns0(); opt != nil {
 		for _, option := range opt.Option {
 			if option.Option() == dns.EDNS0SUBNET {
@@ -287,18 +277,25 @@ func (c *Client) findClientIP(w dns.ResponseWriter, r *dns.Msg) (ednsClientAddre
 			}
 		}
 	}
-	remoteAddr, err := net.ResolveUDPAddr("udp", w.RemoteAddr().String())
+	var ip net.IP
+	var err error
+	ip, _, err = net.ParseCIDR(c.conf.EDNSSubnet)
 	if err != nil {
-		return
-	}
-	if ip := remoteAddr.IP; jsonDNS.IsGlobalIP(ip) {
-		if ipv4 := ip.To4(); ipv4 != nil {
-			ednsClientAddress = ipv4.Mask(ipv4Mask24)
-			ednsClientNetmask = 24
-		} else {
-			ednsClientAddress = ip.Mask(ipv6Mask48)
-			ednsClientNetmask = 48
+		remoteAddr, err := net.ResolveUDPAddr("udp", w.RemoteAddr().String())
+		if err != nil {
+			return
 		}
+		ip = remoteAddr.IP
+		if !jsonDNS.IsGlobalIP(ip) {
+			return
+		}
+	}
+	if ipv4 := ip.To4(); ipv4 != nil {
+		ednsClientAddress = ipv4.Mask(ipv4Mask24)
+		ednsClientNetmask = 24
+	} else {
+		ednsClientAddress = ip.Mask(ipv6Mask48)
+		ednsClientNetmask = 48
 	}
 	return
 }
